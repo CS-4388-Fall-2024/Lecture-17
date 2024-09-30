c3e3146d1cb6ae4205f0988e4bf78ba7bfebfe08
Isayas Adhanom <isayasadhanom@gmail.com>
2024-09-29 22:17:04 -0500

    Initial commit

---
+++ src/App.ts
@@ -0,0 +1,80 @@
+/* Lecture 17: Animation and Kinematics
+ * CS 4388/ CS 5388, Fall 2024, Texas State University
+ * Instructor: Isayas Berhe Adhanom <isayas@txstate.edu>
+ * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ */ 
+
+import * as gfx from 'gophergfx'
+import { GUI } from 'dat.gui'
+import { RobotArm } from './RobotArm'
+
+export class App extends gfx.GfxApp
+{
+    private cameraControls: gfx.OrbitControls;
+    private robotArm: RobotArm;
+
+    // --- Create the App class ---
+    constructor()
+    {
+        // initialize the base class gfx.GfxApp
+        super();
+
+        this.cameraControls = new gfx.OrbitControls(this.camera);
+        this.robotArm = new RobotArm();
+    }
+
+
+    // --- Initialize the graphics scene ---
+    createScene(): void 
+    {
+        // Setup camera
+        this.camera.setPerspectiveCamera(60, 1920/1080, .1, 20)
+        this.cameraControls.setTargetPoint(new gfx.Vector3(0, 0.75, 0));
+        this.cameraControls.setDistance(1.6);
+
+        // Set a black background
+        this.renderer.background.set(0, 0, 0);
+        
+        // Create an ambient light
+        const ambientLight = new gfx.AmbientLight(new gfx.Vector3(0.3, 0.3, 0.3));
+        this.scene.add(ambientLight);
+
+        // Create a directional light
+        const directionalLight = new gfx.DirectionalLight(new gfx.Vector3(0.6, 0.6, 0.6));
+        directionalLight.position.set(2, 1, 3)
+        this.scene.add(directionalLight);
+
+        // Create a grid for the ground plane
+        const gridSize = 10;
+        const gridVertices: gfx.Vector3[] = [];
+        //const gridColors: gfx.Color[] = [];
+        for(let i=-gridSize/2; i <= gridSize/2; i++)
+        {
+            gridVertices.push(new gfx.Vector3(-gridSize/2, 0, i));
+            gridVertices.push(new gfx.Vector3(gridSize/2, 0, i));
+            gridVertices.push(new gfx.Vector3(i, 0, -gridSize/2));
+            gridVertices.push(new gfx.Vector3(i, 0, gridSize/2));
+
+        //     gridColors.push(new gfx.Color(Math.random(), Math.random(), Math.random()));
+        //     gridColors.push(new gfx.Color(Math.random(), Math.random(), Math.random()));
+        //     gridColors.push(new gfx.Color(Math.random(), Math.random(), Math.random()));
+        //     gridColors.push(new gfx.Color(Math.random(), Math.random(), Math.random()));
+        }
+
+        const gridLines = new gfx.Line3(gfx.LineMode3.LINES);
+        gridLines.setVertices(gridVertices);
+        //gridLines.setColors(gridColors);
+        this.scene.add(gridLines);
+
+        this.robotArm.createHierarchy();
+        this.robotArm.createGeometry();
+        this.scene.add(this.robotArm);
+    }
+
+    
+    // --- Update is called once each frame by the main graphics loop ---
+    update(deltaTime: number): void 
+    {
+        this.cameraControls.update(deltaTime);
+    }
+}
---
+++ src/RobotArm.ts
@@ -0,0 +1,95 @@
+/* Lecture 17: Animation and Kinematics
+ * CS 4388/ CS 5388, Fall 2024, Texas State University
+ * Instructor: Isayas Berhe Adhanom <isayas@txstate.edu>
+ * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ */ 
+
+import * as gfx from 'gophergfx'
+
+export class RobotArm extends gfx.Skeleton
+{
+    public showAxes: boolean;
+
+    constructor()
+    {
+        super();
+
+        this.showAxes = false;
+    }
+
+    public createHierarchy(): void
+    {
+        const upperArm = new gfx.Bone();
+        upperArm.name = "upperArm";
+        upperArm.direction.set(0, 1, 0);
+        upperArm.length = 0.5;
+        this.add(upperArm);
+
+        const middleArm = new gfx.Bone();
+        middleArm.name = "middleArm";
+        middleArm.direction.set(0, 1, 0);
+        middleArm.length = 0.4;
+
+        middleArm.position.copy(upperArm.direction);
+        middleArm.position.multiplyScalar(upperArm.length);
+        upperArm.add(middleArm);
+
+        const lowerArm = new gfx.Bone();
+        lowerArm.name = "lowerArm";
+        lowerArm.direction.set(0, 1, 0);
+        lowerArm.length = 0.4;
+
+        lowerArm.position.copy(middleArm.direction);
+        lowerArm.position.multiplyScalar(middleArm.length);
+        middleArm.add(lowerArm);
+    }
+
+    public createGeometry(): void
+    {
+        this.children.forEach((child: gfx.Node3) => {
+            if(child instanceof gfx.Bone)
+            {
+                this.createGeometryRecursive(child);
+            }
+        });
+    }
+
+    public createGeometryRecursive(bone: gfx.Bone): void
+    {
+        if(this.showAxes)
+        {
+            const axes = gfx.Geometry3Factory.createAxes(0.25);
+            bone.add(axes);
+        }
+        else if(bone.name == 'upperArm')
+        {
+            const armMesh = gfx.Geometry3Factory.createBox(0.05, bone.length, 0.05);
+            armMesh.position.set(0, bone.length/2, 0);
+            armMesh.material.setColor(new gfx.Color(0, 1, 0));
+            bone.add(armMesh);
+        }
+        else if(bone.name == 'middleArm')
+        {
+            const armMesh = gfx.Geometry3Factory.createBox(0.05, bone.length, 0.05);
+            armMesh.position.set(0, bone.length/2, 0);
+            armMesh.material.setColor(new gfx.Color(0, 0, 1));
+            bone.add(armMesh);
+
+            
+        }
+        else if(bone.name == 'lowerArm')
+        {
+            const armMesh = gfx.Geometry3Factory.createBox(0.05, bone.length, 0.05);
+            armMesh.position.set(0, bone.length/2, 0);
+            armMesh.material.setColor(new gfx.Color(1, 0, 0));
+            bone.add(armMesh);
+        }
+
+        bone.children.forEach((child: gfx.Node3) => {
+            if(child instanceof gfx.Bone)
+            {
+                this.createGeometryRecursive(child);
+            }
+        });
+    }
+}
